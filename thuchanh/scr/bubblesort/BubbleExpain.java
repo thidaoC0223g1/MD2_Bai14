2b992e403c56ea4bc82c8c50c8c4eff930b95f4b
thidaoC0223g1 <daogiathi231290@gmail.com>
2023-04-10 11:20:27 +0700

    xong bubble 2

--- thuchanh/scr/bubblesort/BubbleExpain.java
+++ thuchanh/scr/bubblesort/BubbleExpain.java
@@ -1,5 +1,7 @@
 package bubblesort;
 
+import java.util.Scanner;
+
 public class BubbleExpain {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -11,8 +13,35 @@ public class BubbleExpain {
             list[i] = scanner.nextInt();
         }
         System.out.print("Your input list: ");
-        for (int i = 0; i < list.length; i++) {
-            System.out.print(list[i] + "\t");
+        for (int j : list) {
+            System.out.print(j + "\t");
+        }
+        System.out.println("\nBegin sort processing...");
+        bubbleSortByStep(list);
+    }
+    public static void bubbleSortByStep(int[] list) {
+        boolean needNextPass = true;
+        for (int k = 1; k < list.length && needNextPass; k++) {
+            needNextPass = false;
+            for (int i = 0; i < list.length - k; i++) {
+                if (list[i] > list[i + 1]) {
+                    /* Swap list[i] with list[i + 1] */
+                    System.out.println("Swap " + list[i] + " with " + list[i + 1]);
+                    int temp = list[i];
+                    list[i] = list[i + 1];
+                    list[i + 1] = temp;
+
+                    needNextPass = true;
+                }
+            }
+            if (!needNextPass) {
+                System.out.println("Array may be sorted and next pass not needed");
+                break;
+            }
+            /* Show the list after sort */
+            System.out.print("List after the  " + k + "' sort: ");
+            for (int i : list) System.out.print(i + "\t");
+            System.out.println();
         }
     }
 }
