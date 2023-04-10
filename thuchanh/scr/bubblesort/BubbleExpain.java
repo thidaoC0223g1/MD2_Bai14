5069ed6aa135db7689e7bf5867748a5e4629eead
thidaoC0223g1 <daogiathi231290@gmail.com>
2023-04-10 11:07:57 +0700

    xong sap xep noi bot 2

---
+++ thuchanh/scr/bubblesort/BubbleExpain.java
@@ -0,0 +1,18 @@
+package bubblesort;
+
+public class BubbleExpain {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter list size:");
+        int size = scanner.nextInt();
+        int[] list = new int[size];
+        System.out.println("Enter " + list.length + " values:");
+        for (int i = 0; i < list.length; i++) {
+            list[i] = scanner.nextInt();
+        }
+        System.out.print("Your input list: ");
+        for (int i = 0; i < list.length; i++) {
+            System.out.print(list[i] + "\t");
+        }
+    }
+}
